08f9787e315b483c63f1538bf882b22fd89bee25
victorcosmedev <victorcosme.profissional@gmail.com>
2025-11-11 19:15:50 -0300

    feat: user entity

---
+++ ZenLogAPI.Domain/Entities/UserEntity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZenLogAPI.Domain.Entities
+{
+    [Table("tb_zl_user")]
+    public class UserEntity
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Username { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
