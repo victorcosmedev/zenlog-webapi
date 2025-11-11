b1bd192340d9a7d3e9f966b0b509ba3fc71ada0e
victorcosmedev <victorcosme.profissional@gmail.com>
2025-11-11 20:02:50 -0300

    feat: application context

---
+++ ZenLogAPI.Infra.Data/AppData/ApplicationContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZenLogAPI.Domain.Entities;
+
+namespace ZenLogAPI.Infra.Data.AppData
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+        }
+
+        public DbSet<ColaboradorEntity> Colaboradores { get; set; }
+        public DbSet<EmpresaEntity> Empresas { get; set; }
+        public DbSet<LogEmocionalEntity> LogsEmocionais { get; set; }
+
+    }
+}
