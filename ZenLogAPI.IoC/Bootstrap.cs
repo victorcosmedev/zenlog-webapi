6441461c8ec3f95c5b026ac880c1c9c9bfeaa783
victorcosmedev <victorcosme.profissional@gmail.com>
2025-11-12 21:26:34 -0300

    feat: bootstrap

---
+++ ZenLogAPI.IoC/Bootstrap.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZenLogAPI.Application.Interfaces;
+using ZenLogAPI.Application.Services;
+using ZenLogAPI.Domain.Interfaces;
+using ZenLogAPI.Infra.Data.AppData;
+using ZenLogAPI.Infra.Data.Repositories;
+
+namespace ZenLogAPI.IoC
+{
+    public class Bootstrap
+    {
+        public static void AddIoC(IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = "Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=oracle.fiap.com.br)(PORT=1521)))(CONNECT_DATA=(SERVER=DEDICATED)(SID=ORCL)));User Id=rm558856;Password=fiap25;";
+            services.AddDbContext<ApplicationContext>(opt => opt.UseOracle(connectionString));
+
+            services.AddTransient<IEmpresaApplicationService, EmpresaApplicationService>();
+            services.AddTransient<IEmpresaRepository, EmpresaRepository>();
+
+            services.AddTransient<ILogEmocionalApplicationService, LogEmocionalApplicationService>();
+            services.AddTransient<ILogEmocionalRepository, LogEmocionalRepository>();
+
+            services.AddTransient<IColaboradorApplicationService, ColaboradorApplicationService>();
+            services.AddTransient<IColaboradorRepository, ColaboradorRepository>();
+        }
+    }
+}
