e6cb699c87fabc1a4c50ad0260f84e8d5496a7dc
victorcosmedev <victorcosme.profissional@gmail.com>
2025-11-13 23:20:20 -0300

    LogEmocionalRepositoryTest

---
+++ ZenLogAPI.Test/App/LogEmocionalRepositoryTest.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZenLogAPI.Domain.Entities;
+using ZenLogAPI.Infra.Data.AppData;
+using ZenLogAPI.Infra.Data.Repositories;
+
+namespace ZenLogAPI.Test.App;
+
+public class LogEmocionalRepositoryTest
+{
+    private ApplicationContext CreateContext()
+    {
+        var options = new DbContextOptionsBuilder<ApplicationContext>()
+            .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+            .Options;
+
+        var context = new ApplicationContext(options);
+        context.Database.EnsureCreated();
+        return context;
+    }
+
+    [Fact]
+    [Trait("Repository", "LogEmocional")]
+    public async Task AdicionarAsync_DeveRetornarNull_QuandoColaboradorNaoExiste()
+    {
+        await using var context = CreateContext();
+        var repository = new LogEmocionalRepository(context);
+
+        var novoLogEmocional = new LogEmocionalEntity
+        {
+            ColaboradorId = int.MaxValue,
+            NivelEmocional = NivelEmocional.Mal,
+            DescricaoSentimento = "Me sinto estressado",
+            FezExercicios = false,
+            HorasDescanso = 4,
+            LitrosAgua = 1,
+            CreatedAt = DateTime.UtcNow
+        };
+
+        var logSalvo = await repository.AdicionarAsync(novoLogEmocional);
+
+        Assert.Null(logSalvo);
+    }
+}
